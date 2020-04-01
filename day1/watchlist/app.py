79505804a092a85a33ba346689b644ab6a93c78d
zhangqi <1015784643@qq.com>
2020-04-01 16:38:34 +0800

    第三次

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -2,8 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+# @app.route('/index')
+# @app.route('/home')
 
 def index():
-    return "<h1>hello,Flask</h1>"
+    return "<h1>hello,Flask,00</h1>"
 
-
+# 动态url
+@app.route('/index/<name>')
+def home(name):
+    return "<h1>hello,Flask,%s</h1>" %name
