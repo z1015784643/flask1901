93a96c9479f0111cd959466d4508955d12396724
zhangqi <1015784643@qq.com>
2020-04-02 16:12:16 +0800

    数据库中取数据

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -16,7 +16,8 @@ app = Flask(__name__)
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////'+os.path.join(app.root_path,'data.db')
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path,'data.db')
-app.config ['SQLALCHY_TRACK_MODIFICATIONS'] = Flask #关闭对模型修改的监控
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False    #关闭对模型修改的监控
 db = SQLAlchemy(app)  #初始化扩展，传入程序实例app
 
 class User(db.Model):
@@ -33,14 +34,9 @@ class Movies(db.Model):
 # @app.route('/home')
 
 def index():
-    name = "中国"
-    movies = [
-        { "title" : "大赢家" , "year" : "2020" },
-        { "title" : "叶问四" , "year" : "2020" },
-        { "title" : "唐人街探案" , "year" : "2020" },
-        { "title" : "囧妈" , "year" : "2020" },
-    ]
-    return render_template('index.html',name=name,movies=movies)
+    user = User.query.first()
+    movies = Movies.query.all()
+    return render_template('index.html',name=user.name,movies=movies)
 
 # # 动态url
 # @app.route('/index/<name>')
@@ -49,6 +45,7 @@ def index():
 
 
 # 自定义命令
+# 新建data,db 的数据库初始化命令
 @app.cli.command()   #这个装饰器可以注册命令
 @click.option('--drop',is_flag = True,help = '删除后再创建')
 def initdb(drop):
@@ -57,6 +54,25 @@ def initdb(drop):
 
     db.create_all()
     click.echo('初始化数据库完成')
+
+# 向data.db中写入数据的命令
+
+@app.cli.command()
+def forge():
+    name = "中国"
+    movies = [
+        { "title" : "大赢家" , "year" : "2020" },
+        { "title" : "叶问四" , "year" : "2020" },
+        { "title" : "唐人街探案" , "year" : "2020" },
+        { "title" : "囧妈" , "year" : "2020" },
+    ]
+
+    user = User(name = name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movies(title = m['title'],year = m['year'])
+        db.session.add(movie)
+    db.session.commit()
 
 
 
