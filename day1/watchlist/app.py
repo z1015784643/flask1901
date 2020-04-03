0d95ea92211cf01bfae5831e7c2c16c94493a974
zhangqi <1015784643@qq.com>
2020-04-03 17:44:01 +0800

    登录登出

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -4,8 +4,10 @@ import sys
 import click
 from flask import Flask
 from flask import render_template,flash,redirect,request,url_for
-
+from werkzeug.security import generate_password_hash,check_password_hash
 from flask_sqlalchemy import SQLAlchemy  #导入扩展类
+from flask_login import LoginManager,UserMixin,login_user,logout_user
+
 
 WIN = sys.platform.startswith('win')
 if WIN:
@@ -15,15 +17,35 @@ else:
 
 app = Flask(__name__)
 
+
+
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////'+os.path.join(app.root_path,'data.db')
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path,'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False   #关闭对模型修改的监控
 app.config['SECRET_KEY'] = 'watchlist_dev'
 db = SQLAlchemy(app)  #初始化扩展，传入程序实例app
+login_manager = LoginManager(app)  #实例化登录扩展类
 
-class User(db.Model):
+
+@login_manager.user_loader
+def load_user(user_id):
+    user = User.query.get(int(user_id))
+    return user
+
+
+
+
+# model
+class User(db.Model,UserMixin):
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(20))
+    username = db.Column(db.String(20))
+    password_hash = db.Column(db.String(128))
+    def set_password(self,password):
+        self.password_hash = generate_password_hash(password)
+    def validate_password(self,password):
+        return check_password_hash(self.password_hash,password)
+
 
 class Movies(db.Model):
     id = db.Column(db.Integer,primary_key = True)
@@ -85,6 +107,37 @@ def delete(movie_id):
     flash("删除完成")
     return redirect(url_for('index'))
 
+
+
+# 登录
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if not username or not password:
+            flash('错误')
+            return redirect(url_for('login'))
+        user = User.query.first()
+        # 验证信息
+        if username == user.username and user.validate_password(password):
+            login_user(user)
+            flash('登录成功')
+            return redirect(url_for('index'))
+        flash('验证错误')
+        return redirect(url_for('login'))
+    return render_template('login.html')
+
+
+
+# 登出
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash('拜拜')
+    return redirect(url_for('index'))
+
+
 # # 动态url
 # @app.route('/index/<name>')
 # def home(name):
@@ -124,6 +177,25 @@ def forge():
         db.session.add(movie)
     db.session.commit()
 
+
+# 生成管理员账号
+@app.cli.command()
+@click.option('--username',prompt=True,help="管理员账号")
+@click.option('--password',prompt=True,help="管理员密码" ,hide_input=True,confirmation_prompt=True)
+def admin(username,password):
+    db.create_all()
+    user = User.query.first()
+    if user is not None:
+        click.echo('更新用户信息')
+        user.username = username 
+        user.set_password(password)
+    else:
+        click.echo('创建用户信息')
+        user = User(username=username,name='Admin')
+        user.set_password(password)
+        db.session.add(user)
+    db.session.commit()
+    click.echo('管理员创建完成')
 
 # 错误处理函数
 @app.errorhandler(404)
